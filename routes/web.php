bb7826944e6f0cefd78ac575bac09ef8c1c1fef2
Patrick Ciudad <PatrickWCity@gmail.com>
2019-09-04 04:18:44 -0400

    Login, ACL básico, mix y Heroku

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles','RoleController');
+    Route::resource('users','UserController');
+    Route::resource('products','ProductController');
+});
